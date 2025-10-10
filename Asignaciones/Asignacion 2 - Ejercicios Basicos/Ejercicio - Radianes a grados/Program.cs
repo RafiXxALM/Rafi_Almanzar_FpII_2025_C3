e12b4ea4eb603a535bd0980c0e39781a01535a8c
RafiXxALM <rafiyunior@gmail.com>
2025-10-10 15:59:12 -0400

    Agregado de ejercicio a Asignacion 2

---
+++ Asignaciones/Asignacion 2 - Ejercicios Basicos/Ejercicio - Radianes a grados/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Escribir un programa que convierta angulos de grados a radianes y viceversa
+        // r= g*pi/180
+        // g= r*180/pi
+
+        // Datos de entrada
+        // Angulo a convertir, double, 0
+        // seleccion, string, ""
+        double angulo = 0;
+        string seleccion = "";
+
+        // Datos de trabajo
+        // valor, string, ""
+        // pi, double, 3.14159
+        string valor = "";
+        double pi = 3.14159;
+
+        // Datos de salida
+        // radianes, double, 0
+        // grados, double, 0
+        double radianes = 0;
+        double grados = 0;
+
+        // Pedir si es grados a radianes o radianes a grados
+        Console.WriteLine("1- radianes a grados, 2- grados a radianes ");
+        seleccion = Console.ReadLine();
+
+
+        // pedir angulo
+        Console.WriteLine("Ingrese el angulo a convertir: ");
+        valor = Console.ReadLine();
+        angulo = Convert.ToDouble(valor);
+
+        // Convertir radianes a grados
+        if (seleccion == "1")
+        {
+            grados = angulo * 180 / pi;
+            // Mostrar resultado
+            Console.WriteLine("{0} radianes son {1} grados", angulo, grados);
+        }
+
+
+        // Convertir grados a radianes
+        if (seleccion == "2")
+        {
+            radianes = angulo * pi / 180;
+            Console.WriteLine("{0} grados son {1} radianes", grados, angulo);
+        }
+    }
+}
---
+++ Asignaciones/Asignacion 2 - Ejercicios Basicos/Ejercicio - Raíz cuadrada/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Escribir un programa que calcule la raiz cuadrada de cualquier numero.
+        // Se calcula con Math.Sqrt()
+
+        // Datos de entrada
+        // numero, 0, double
+        double numero = 0;
+
+        // Dato de trabajo
+        // string, valor, ""
+        string valor = "";
+
+        // Dato de salida
+        // raiz, 0, double
+        double raiz = 0;
+
+        // Pedir el numero
+        Console.WriteLine("Ingrese un numero para calcular su raiz cuadrada:");
+        valor = Console.ReadLine();
+        numero = Convert.ToDouble(valor);
+
+        // Calcular la raiz
+        raiz = Math.Sqrt(numero);
+
+        // Mostrar la raiz
+        Console.WriteLine("La raiz cuadrada de " + numero + " es: " + raiz);
+    }
+}
